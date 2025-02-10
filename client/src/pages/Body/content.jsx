8900d8cfe430d7bf0436513bd627ea9c9095cf42
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-02-10 22:50:40 +0700

    complete home page

---
+++ client/src/pages/Body/content.jsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/Footer/footer";
+
+
+function Content() {
+    return (
+        <>
+            <div className="container mx-auto px-[6rem]">
+
+                <Footer />
+            </div>
+        </>
+    )
+}
+export default Content;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+
+import Header from "../../components/Header/Header";
+import Content from "../Body/content";
+
+
 function Home() {
     return (
         <>
-        
+            <Header />
+            <Content />
         </>
     )
 }
