4fbf6f10ce3e0cec5445e42eab44400acb3ba3f4
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-04-06 00:34:23 +0700

    update

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://libary-software-ucin.vercel.app",
+  baseURL: "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   }
